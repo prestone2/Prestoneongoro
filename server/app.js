8dd4c30a2e212424a9f318a7493ce1c88eee6a5d
prestone2 <prestoneongoro@gmail.com>
2025-01-02 13:49:55 +0300

    deleted:    node_modules/.package-lock.json 	deleted:    package-lock.json 	modified:   server/app.js 	modified:   server/node_modules/.package-lock.json 	modified:   server/node_modules/express/History.md 	modified:   server/node_modules/express/package.json 	modified:   server/node_modules/path-to-regexp/index.js 	modified:   server/node_modules/path-to-regexp/package.json 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/app.js
+++ server/app.js
@@ -16,4 +16,14 @@ app.use(cors({
 // Serve static files from the 'Public' folder within the server directory
 app.use(express.static(path.join(__dirname, 'Public')))
 
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Public', 'index.html'))
+})
+
+// Handle 404 errors
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'Public', '404.html'))
+})
+
 app.listen(4000, () => console.log('server listening on http://localhost:4000'))
